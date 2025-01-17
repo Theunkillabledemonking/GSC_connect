460a40eadfdef7a5fc962555bd7f27456483c72e
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-18 02:24:04 +0900

    사소한 오류 수정..

--- src/config/database.php
+++ src/config/database.php
@@ -1,13 +1,16 @@
 <?php
 $host = '127.0.0.1';      // 호스트
 $username = 'root';       // 사용자명
+$port = '3306';
 $password = 'gsc1234!@#$';           // 비밀번호 (기본적으로는 비어 있음)
-$database = 'school_portal'; // 데이터베이스 이름
+$database = 'users'; // 데이터베이스 이름
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
 // 연결 오류 확인
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die('Database connection failed: ' . $conn->connect_error);
+} else {
+    echo "Database connected successfully!";
 }
 ?>
