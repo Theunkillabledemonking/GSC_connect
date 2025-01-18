f743b74d4d32b9868c356f8c8d2f7dad03fdc0ff
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-19 01:12:31 +0900

    server up.

--- public/index.php
+++ public/index.php
@@ -1,22 +1,11 @@
 <?php
 $pageTitle = "로그인";
 
-// 절대경로 기반으로 헤더와 푸터 불러오기
+// 절대경로 기반으로 헤더 불러오기
 require_once(dirname(__DIR__, 2) . '/includes/header.php');
-?>
 
-<div class="container">
-    <h1>GSC Portal</h1>
-    <div class="form-container">
-        <form action="./views/process_login.php" method="POST">
-            <input type="text" name="student_id" placeholder="학번" required>
-            <input type="password" name="password" placeholder="비밀번호" required>
-            <button type="submit">LOG IN</button>
-        </form>
-        <div class="signup-link">
-            계정이 없으신가요? <a href="./views/signup.php">Sign Up</a>
-        </div>
-    </div>
-</div>
+// HTML 파일 불러오기
+include_once(dirname(__DIR__) . '/public/html/login.html');
 
-<?php require_once(dirname(__DIR__, 2) . '/includes/footer.php'); ?>
+// 절대경로 기반으로 푸터 불러오기
+require_once(dirname(__DIR__, 2) . '/includes/footer.php');
