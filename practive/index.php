a596908c21c3e6bc5b372692b5fb7e0b314e4944
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-18 22:49:00 +0900

    연습파일.

---
+++ practive/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 프로그래밍 입문</title>
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <header>
+        <?php include "header.php";?>
+    </header>
+    <section>
+        <?php include "main.php";?>
+    </section>
+    <footer>
+        <?php include "footer.php";?>
+    </footer>
+    
+</body>
+</html>
