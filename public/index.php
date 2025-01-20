21dee07efd41919cfb9253737b2fc8eab26ec4e7
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-20 22:25:44 +0900

    로그인 오류 추가.

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ session_start();
 
 // 로그인 여부 확인 (세션 기반으로 검증)
 if (!isset($_SESSION['student_id'])) {
-    header("Location: ./login.php");
+    header("Location: ./index.php");
     exit;
 }
 
@@ -15,6 +15,14 @@ $student_id = htmlspecialchars($_SESSION['student_id']);
 $name = htmlspecialchars($_SESSION['name']);
 $role = htmlspecialchars($_SESSION['role']);
 
-// 메인 화면 포함
+// 절대경로 기반으로 헤더 불러오기
+require_once(dirname(__DIR__, 1) . '/includes/header.php');
+
+// HTML 파일 불러오기
 include_once(dirname(__DIR__, 1) . '/views/main.php');
+
+// 절대경로 기반으로 푸터 불러오기
+require_once(dirname(__DIR__, 1) . '/includes/footer.php');
+
+
 ?>
