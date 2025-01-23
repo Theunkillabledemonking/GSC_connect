88c5e44f91fc00811ff689073c33b604a1a48d08
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-24 01:29:49 +0900

    코드 수정 후 커밋.

--- practive/user_addres.php
+++ practive/user_addres.php
@@ -5,91 +5,61 @@
 </form>
 
 <?php
-if((isset($_POST['rrn']))) {
+if (isset($_POST['rrn'])) {
     // 주민번호 입력값
     $rrn = $_POST['rrn'];
-    // 하이폰을 지우고 재정렬한 배열
-    $clear_array = [];
-    // 합계
-    $total_num = 0;
-    // 검사 배열
-    $check_array = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];
 
+    // 하이폰 제거
+    $cleaned_rrn = '';
     for ($i = 0; $i < strlen($rrn); $i++) {
         if ($rrn[$i] !== "-"){
-        $clear_array[] = $rrn[$i];
+            $cleaned_rrn .= $rrn[$i];
         }
     }
     
-    $length = 0;
+    // 길이 확인
+    if (strlen($cleaned_rrn) !== 13) {
+        echo "주민번호는 13자리여야 합니다.";
+        exit;
+    }
     
-    for ($i = 0; $i < count($clear_array); $i++) {
-        $length++;
-    }
+    // 생년월일 유효성 검사
+    $year = (int)($cleaned_rrn[0] . $cleaned_rrn[1]);
+    $month = (int)($cleaned_rrn[2] . $cleaned_rrn[3]);
+    $day = (int)($cleaned_rrn[4] . $cleaned_rrn[5]);
 
-    if ($length !== 13) {
-        echo "13자리가 아닙니다.";
-        exit;
-    } 
-    
-    // 연도
-    $year = ($clear_array[0] . $clear_array[1]);    
-    if (!(0 <= $year && $year <= 99)) {
-        echo "잘못된 년도입니다.";
-        exit;
-    }
-
-    // 월
-    $month = ($clear_array[2] . $clear_array[3]);
-    if (!(1 <= $month && $month <= 12)) {
-        echo "잘못된 월입니다.";
-        exit;
-    }
-
-    // 일
-    $day = ($clear_array[4] . $clear_array[5]);
-    if (!(1 <= $day && $day <= 31)) {
-        echo "잘못된 날짜입니다.";
+    if ($month < 1 || $month > 12 || $day < 1 || $day > 31) {
+        echo "유효하지 않은 생년월일입니다.";
         exit;
     }
 
     // 젠더 판별
-    $gender_check = $clear_array[6];
-    $gender;
-    switch($gender_check) {
-        case "1" :
-        case "3" :
-            $gender = "남성";
-            break;
-        case "2":
-        case "4":
-            $gender = "여성";
-            break;
-        default:
-            echo "유효하지 않는 값입니다.";
-            exit;
-    }
-
-    for ($i = 0; $i < $length - 1; $i++) {
-        $total_num += (int)$clear_array[$i] * $check_array[$i];
-    }
-
-    $multi = (11 - $total_num % 11)%10;
-    
-    if ((int)$clear_array[12] === $multi) {
-        echo "입력하신 주민등록번호 $gender ";
-        for ($i = 0; $i < 6; $i++) {
-            echo $clear_array[$i];
-        }
-        echo "-$clear_array[6]";
-        for ($i = 0; $i < 7; $i++) {
-            echo "*";
-        }
-        echo "은(는) 유효합니다.";
-    } else {
-        echo "유효하지않는 주민먼호";
+    $gender = $cleaned_rrn[6];
+    if ($gender !== '1' && $gender !== '2' && $gender !== '3' && $gender !== '4') {
+        echo "유효하지 않은 성별 코드입니다.";
         exit;
     }
+
+    // 검사 배열
+    $check_array = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5];
+
+    $total_num = 0;
+    for ($i = 0; $i < 12; $i++) {
+        $total_num += (int)$cleaned_rrn[$i] * $check_array[$i];
+    }
+
+    $check_digit = (11 - $total_num % 11)%10;
+    
+    if ((int)$cleaned_rrn[12] !== $check_digit) {
+        echo "유효하지 않은 주민등록번호입니다.";
+        exit;
+    }
+
+    // 결과 출력
+    $gender_label = ($gender === '1' || $gender == '3') ? "남성" : "여성";
+    echo "입력하신 주민등록번호는 {$gender_label}이며 유효합니다.";
+    echo substr($cleaned_rrn, 0, 6) . "-";
+    echo $gender . str_repeat("*", 6);
 
 } else {
     echo "값을 입력해주세요";
