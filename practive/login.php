ac9e68c46bea1a48cd9b003be68546d7a60e01bf
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-25 21:37:57 +0900

    간단한 로그인/회원가입 코드 작성.

---
+++ practive/login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+$conn = new mysqli('localhost', 'root', '', 'school_portal');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($id, $hased_password);
+
+    if ($stmt->fetch() && password_verify($password, $hased_password)) {
+        $_SESSION['user_id'] = $id;
+        echo "로그인 성공!";
+    } else {
+        echo "로그인 실패: 아이디 또는 비밀번호를 확인하세요.";
+    }
+
+    $stmt->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="username" placeholder="아이디" require>
+        <input type="password" name="password" placeholder="비밀번호" require>
+        <button type="submit">로그인</button>
+    </form>
+</body>
+</html>
