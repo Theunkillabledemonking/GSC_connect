d2b2f9ed3fa4cadc621217da93a08f4f253005ac
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-01-25 18:21:03 +0900

    마지막 작업 완료.

--- practive/index.php
+++ practive/index.php
@@ -3,20 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP 프로그래밍 입문</title>
-    <link rel="stylesheet" type="text/css" href="./css/common.css">
-    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <title>프로그래밍 입문</title>
+    
 </head>
 <body>
     <header>
-        <?php include "header.php";?>
+        <?php include = "header.php";?>
     </header>
     <section>
-        <?php include "main.php";?>
-    </section>
+        <?php include = "main.php";?>
+    </setion>
     <footer>
-        <?php include "footer.php";?>
+        <?php include = "footer.php";?>
     </footer>
-    
 </body>
 </html>
